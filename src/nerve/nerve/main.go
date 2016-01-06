40c0df77695ed36395bf9d7a34be8b13d99ea9c0
Nicolas Blanc <sinfomicien@gmail.com>
2016-01-06 22:24:23 +0100

    Refactor to clean the code

--- src/nerve/nerve/main.go
+++ src/nerve/nerve/main.go
@@ -9,11 +9,10 @@ import (
 	"flag"
 )
 
-// The function which is used to manageSignal
+// Manage OS Signal, only for shutdown purpose
 // When termination signal is received, we send a message to a chan
-func manageSignal(c <-chan os.Signal, working chan<-bool) {
-        toBreak := true
-        for i := 0; toBreak; i=i+1 {
+func manageSignal(c <-chan os.Signal, stop chan<-bool) {
+        for {
 		select {
 		case _signal := <-c:
 			if _signal == os.Kill {
@@ -23,8 +22,8 @@ func manageSignal(c <-chan os.Signal, working chan<-bool) {
 				log.Debug("Nerve: Interrupt Signal Received")
 			}
 			log.Info("Nerve: Shutdown Signal Received")
-			working <- false
-			toBreak = false
+			stop <- true
+			break
 		default:
 			time.Sleep(time.Millisecond * 100)
 		}
@@ -47,7 +46,8 @@ func setLogLevel(logLevel string) {
 	}
 }
 
-func main() {
+// All the command line arguments are managed inside this function
+func initFlags() (string, string) {
 	// The Log Level, from the Sirupsen/logrus level
 	var logLevel = flag.String("log-level", "WARN", "A value to choose between [DEBUG INFO WARN FATAL], can be overriden by config file")
 	// The configuration filename
@@ -55,29 +55,49 @@ func main() {
 	// Parse all command line options
 	flag.Parse()
 
-	setLogLevel(*logLevel)
+	return *logLevel, *configurationFileName
+}
 
+func initConfiguration(configurationFileName string) (nerve.NerveConfiguration) {
 	log.Debug("Nerve: Starting config file parsing")
-	nerveConfiguration , err := nerve.OpenConfiguration(*configurationFileName)
+	nerveConfiguration , err := nerve.OpenConfiguration(configurationFileName)
 	if err != nil {
 		// If an error is raised when parsing configuration file
 		// the configuration object can be either empty, either incomplete
 		log.Fatal("Nerve: Unable to load Configuration (",err,")")
-		// So of the configuration is incomplete, exit the program now
+		// So the configuration is incomplete, exit the program now
 		os.Exit(1)
 	}
+	log.Debug("Nerve: Config file parsed")
+	return nerveConfiguration
+}
+
+func main() {
+	// Init flags, to get logLevel and configuration file name
+	logLevel, configurationFileName := initFlags()
+
+	setLogLevel(logLevel)
+
+	// Load the configuration from config file. If something wrong, the full process is stopped inside the function
+	nerveConfiguration := initConfiguration(configurationFileName)
+
+	// If the log level is setted inside the configuration file, override the command line level
 	if nerveConfiguration.LogLevel != "" {
 		setLogLevel(nerveConfiguration.LogLevel)
 	}
+
 	log.Info("Nerve: Starting Run of Instance [",nerveConfiguration.InstanceID,"]")
+
 	c := make(chan os.Signal,1)
 	signal.Notify(c, os.Interrupt, os.Kill)
 	log.Debug("Nerve: Signal Channel notification setup done")
-	working := make(chan bool)
-	go manageSignal(c,working)
+
+	stop := make(chan bool)
+	go manageSignal(c,stop)
 	log.Debug("Nerve: Signal Management Started")
+
 	finished:= make(chan bool)
-	go nerve.Run(working,finished,nerveConfiguration)
+	go nerve.Run(stop,finished,nerveConfiguration)
 	log.Debug("Nerve: Go routine launched")
 
 	log.Debug("Nerve: Waiting for main process to Stop")
