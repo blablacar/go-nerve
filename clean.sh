792537424220c46988f51d6492c3f65e6e1880fe
Arnaud Lemaire <alemaire@norad.fr>
2016-06-08 11:56:42 +0200

    add gomake

--- clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-dir=$( dirname "$0" )
-
-echo -e "\033[0;32mCleaning\033[0m"
-rm -Rf ${dir}/dist
--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-start=`date +%s`
-dir=$( dirname "$0" )
-
-echo -e "\033[0;32mTesting\033[0m"
-godep go test -cover ${dir}
-
-echo -e "\033[0;35mTest duration : $((`date +%s`-start))s\033[0m"
