c2d2ec76d7186d552ea9dffb36fd1a22eee25177
Arnaud Lemaire <alemaire@norad.fr>
2016-06-23 11:40:55 +0200

    use cobra as argument parse. set config as mandatory argument

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"github.com/blablacar/go-nerve/nerve"
 	"github.com/ghodss/yaml"
@@ -13,38 +12,11 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"github.com/spf13/cobra"
 )
 
 var Version = "No Version Defined"
 var BuildTime = "1970-01-01_00:00:00_UTC"
-
-func printVersion() {
-	fmt.Println("Nerve")
-	fmt.Println("Version :", Version)
-	fmt.Println("Build Time :", BuildTime)
-}
-
-func processArgsGetConfigPath() (string, error) {
-	var logLevel = flag.String("log-level", "info", "log level")
-	var configPath = flag.String("config", "./nerve.yml", "the complete filename of the configuration file")
-	var version = flag.Bool("version", false, "Display version and exit")
-	flag.Parse()
-
-	// version
-	if *version {
-		printVersion()
-		os.Exit(0)
-	}
-
-	// log-level
-	lvl, err := logs.ParseLevel(*logLevel)
-	if err != nil {
-		return "", errs.WithEF(err, data.WithField("input", logLevel), "Invalid log level")
-	}
-	logs.SetLevel(lvl)
-
-	return *configPath, nil
-}
 
 func LoadConfig(configPath string) (*nerve.Nerve, error) {
 	file, err := ioutil.ReadFile(configPath)
@@ -69,21 +41,48 @@ func waitForSignal() {
 }
 
 func main() {
-	configPath, err := processArgsGetConfigPath()
-	if err != nil {
-		logs.WithE(err).Fatal("Argument process failed")
+	var logLevel string
+	var version bool
+
+	rootCmd := &cobra.Command{
+		Use: "nerve config.yml",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			if version {
+				fmt.Println("Nerve")
+				fmt.Println("Version :", Version)
+				fmt.Println("Build Time :", BuildTime)
+				os.Exit(0)
+			}
+
+			level, err := logs.ParseLevel(logLevel)
+			if err != nil {
+				logs.WithField("value", logLevel).Fatal("Unknown log level")
+			}
+			logs.SetLevel(level)
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) != 1 {
+				logs.Fatal("Nerve require a configuration file as argument")
+			}
+			nerve, err := LoadConfig(args[0])
+			if err != nil {
+				logs.WithE(err).Fatal("Cannot start, failed to load configuration")
+			}
+
+			if err := nerve.Init(); err != nil {
+				logs.WithE(err).Fatal("Failed to init nerve")
+			}
+
+			go nerve.Start()
+			waitForSignal()
+			nerve.Stop()
+		},
 	}
 
-	nerve, err := LoadConfig(configPath)
-	if err != nil {
-		logs.WithE(err).Fatal("Cannot start, failed to load configuration")
-	}
+	rootCmd.PersistentFlags().StringVarP(&logLevel, "log-level", "L", "info", "Set log level")
+	rootCmd.PersistentFlags().BoolVarP(&version, "version", "V", false, "Display version")
 
-	if err := nerve.Init(); err != nil {
-		logs.WithE(err).Fatal("Failed to init nerve")
+	if err := rootCmd.Execute(); err != nil {
+		logs.WithE(err).Fatal("Failed to process args")
 	}
-
-	go nerve.Start()
-	waitForSignal()
-	nerve.Stop()
 }
