20ada64000a2887a1f99e9d1d4e99a8c71eb47b3
Arnaud Lemaire <alemaire@norad.fr>
2016-08-04 12:59:33 +0200

    up

--- nerve/report.go
+++ nerve/report.go
@@ -1,6 +1,11 @@
 package nerve
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"bytes"
+	"fmt"
+	"strconv"
+)
 
 type Report struct {
 	Available            *bool             `json:"available"`
@@ -39,4 +44,21 @@ func toReport(status error, s *Service) Report {
 		r.UnavailableReason = status.Error()
 	}
 	return r
+}
+
+func (r *Report) String() string {
+	var buffer bytes.Buffer
+
+	buffer.WriteString(fmt.Sprint(r.Available))
+	buffer.WriteString(" ")
+	buffer.WriteString(r.Name)
+	buffer.WriteString(" ")
+	buffer.WriteString(r.Host)
+	buffer.WriteString(":")
+	buffer.WriteString(strconv.Itoa(r.Port))
+	if r.Weight != nil {
+		buffer.WriteString(" ")
+		buffer.WriteString(strconv.Itoa(int(*r.Weight)))
+	}
+	return buffer.String()
 }
